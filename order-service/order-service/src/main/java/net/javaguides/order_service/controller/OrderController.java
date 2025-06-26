b609805c0f76ec8c9b59d26c666bb9e1f9a33b1f
rahul789147 <rahul15159999@gmail.com>
2025-06-26 14:36:37 +0530

    initial commit with all 3 microservices

---
+++ order-service/order-service/src/main/java/net/javaguides/order_service/controller/OrderController.java
@@ -0,0 +1,39 @@
+package net.javaguides.order_service.controller;
+
+import net.javaguides.base_domains.dto.Order;
+import net.javaguides.base_domains.dto.OrderEvent;
+import net.javaguides.order_service.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer){
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){
+
+    order.setOrderId(UUID.randomUUID().toString());
+
+    OrderEvent orderEvent = new OrderEvent();
+    orderEvent.setStatus("PENDING");
+    orderEvent.setMessage("Order is in Pending state");
+    orderEvent.setOrder(order);
+
+    orderProducer.sendMessage(orderEvent);
+
+    return "Order Placed Successfully";
+
+    }
+
+}
